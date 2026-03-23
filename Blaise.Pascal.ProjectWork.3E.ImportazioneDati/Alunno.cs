55ebf036434f1c581d64f12807f365744389b7e6
Tuo Nome <pietromannu09@gmail.com>
2026-03-23 10:37:20 +0100

    Aggiunta parte di Importazione dati da File excel

---
+++ Blaise.Pascal.ProjectWork.3E.ImportazioneDati/Alunno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Importazione_Dati_Excel
+{
+    // --- DEFINIZIONE DELLE CLASSI ---
+    public class Alunno
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public bool Sesso { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string Cittadinanza { get; set; }
+        public string ComuneResidenza { get; set; }
+        public bool Disabilita { get; set; }
+        public bool Dsa { get; set; }
+        public string Indirizzo { get; set; }
+    }
+}

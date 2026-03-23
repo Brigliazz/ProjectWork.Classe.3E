879758627f75bb4c23c16d161d5bbe2f067d9923
Rusceee <ruscelli.andreas@gmail.com>
2026-03-23 10:39:01 +0100

    Aggiunta la tabella per la scelta delle preferenze e aggiunti gli attributi richiesti per l'alunno

---
+++ BlaisePascal.ProjectWork.3E.Infrastructure/Database/Data/PreferenzaCompagnoRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace BlaisePascal.ProjectWork._3E.Infrastructure.Database.Data
+{
+    internal class PreferenzaCompagnoRepository
+    {
+        private string connectionString = "Data Source=studenti.db";
+
+        public void CreaTabella()
+        {
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            CREATE TABLE IF NOT EXISTS Scelte (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                CodiceFiscaleStudenteScelto TEXT,
+                CodiceFiscaleStudente TEXT,
+                FOREIGN KEY (CodiceFiscaleStudente) REFERENCES Studenti(CodiceFiscale)
+            );
+            ";
+
+            command.ExecuteNonQuery();
+        }
+    }
+}

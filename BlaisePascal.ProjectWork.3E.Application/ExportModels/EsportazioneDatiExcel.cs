6218857af1aceeb91d9a5827f88cdaaa1f15a256
BotrugnoMattia <mattia.botrugno02@gmail.com>
2026-03-27 09:36:45 +0100

    Sistemata Struttura classi e cartelle Dati Esportazione Application Layer

---
+++ BlaisePascal.ProjectWork.3E.Application/ExportModels/EsportazioneDatiExcel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.ProjectWork._3E.Application.ExportModels
+{
+    public class EsportazioneDatiExcel
+    {
+    }
+}
